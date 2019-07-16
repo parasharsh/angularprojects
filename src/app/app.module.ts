012bbcff0cf17bfa77b9f5ab00d598eb902ef650
parashars <parashars@vmware.com>
2019-07-16 11:06:25 +0530

    Updated dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooLibModule } from 'foo-lib';
+import { UserlistComponent } from '#user/userlist/userlist.component';
+import { CustomgridComponent } from '#shared/customgrid/customgrid.component';
+import { SharedModule } from '#shared/shared.module';
+import { UserModule } from '#user/user.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FooLibModule,
+    SharedModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
